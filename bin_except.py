829e8983ec536d5109264658c04db108866e3ff1
jeffrada <72144307+jeffrada@users.noreply.github.com>
2021-01-27 18:40:04 -0500

    Commit 13

--- bin_except.py
+++ bin_except.py
@@ -27,7 +27,7 @@ def binary_search(a_list, target):
 
 def bin_except(num1, num2):
     try:
-        result = 0
+        result = 7
         print(result)
     except TargetNotFound:
         print("Target value not found.")
